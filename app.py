649558d5c30ec8ee9fd78203996f2610e4224f76
James <Jameslilley97@gmail.com>
2021-10-14 16:04:16 +0100

    moved ws to own file

--- app.py
+++ app.py
@@ -143,6 +143,11 @@ def shortinterval_update():
     shortinterval_api_call()
     print("shortinterval_update ran successfully")
 
+#run functions on app startup
+historical_update()
+shortinterval_update()
+
+#run functions every minute/hour
 scheduler = BackgroundScheduler()
 scheduler.add_job(func=print_date_time, trigger="interval", seconds=60)
 scheduler.add_job(func=shortinterval_update, trigger="interval", seconds=60)
@@ -151,54 +156,6 @@ scheduler.start()
 
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
-
-#############
-
-kline_info = {
-        "bitcoin_gbp" : 'btcgbp',
-        "etherium_gbp" : 'ethgbp',
-        "ada_gbp" : 'adagbp',
-        "ripple_gbp" : 'xrpgbp',
-        "solana_gbp" :'solgbp'}
-
-socket = f"wss://stream.binance.com:9443/ws/"
-interval = "1m"
-api_call = ""
-
-for crypto in kline_info:
-    api_info = f"{kline_info[crypto]}@kline_{interval}/"
-    api_call += api_info
-api_request = str(socket+api_call)
-
-api_request = api_request[:-1]
-
-def on_message(ws, message):
-
-    json_message = json.loads(message)
-    candle = json_message['k']
-
-    candlestick_dict = {
-            "time": candle['t'],
-            "symbol": candle['s'],
-            "open": candle['o'],
-            "high": candle['h'],
-            "low": candle['l'],
-            "close": candle['c'],
-        }
-    print(f"live data updated with {candle['s']}")
-    return(candlestick_dict)
-
-
-def on_close(ws):
-    print("Connection closed!")
-
-#create a session to connect with binance websocket server
-ws = websocket.WebSocketApp(api_request, on_message=on_message, on_close=on_close)
-
-#we have to wait 1 minute to get the first data
-ws.run_forever()
-
-###########
 
 # Start Flask app
 if __name__ == '__main__':
